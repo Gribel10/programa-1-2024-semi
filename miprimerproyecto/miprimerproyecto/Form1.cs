a229697e2f7c5de84d41c433157673e7a0cef219
Kelly Gribel Ayala Torres <ayalagribel39@gmail.com>
2024-08-14 19:34:24 -0600

    suma de dos numeros

--- miprimerproyecto/miprimerproyecto/Form1.cs
+++ miprimerproyecto/miprimerproyecto/Form1.cs
@@ -19,7 +19,17 @@ namespace miprimerproyecto
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Console.WriteLine("Hola mundo");
+            //suma de dos numeros 
+            Console.Write("Num1.");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Num 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int respuesta = num1 + num2;
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+
+            //Pausa.
             Console.ReadLine();
 
         }
