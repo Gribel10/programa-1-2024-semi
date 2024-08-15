ccf51e20a77272eb87308ba27c7c118818db6186
Kelly Gribel Ayala Torres <ayalagribel39@gmail.com>
2024-08-14 19:26:16 -0600

    Hola Mundo

--- miprimerproyecto/miprimerproyecto/Form1.cs
+++ miprimerproyecto/miprimerproyecto/Form1.cs
@@ -19,6 +19,8 @@ namespace miprimerproyecto
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Console.WriteLine("Hola mundo");
+            Console.ReadLine();
 
         }
     }
