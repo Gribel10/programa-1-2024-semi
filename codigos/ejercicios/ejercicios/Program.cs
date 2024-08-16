01e7c6d2bb404e970974a8bbef8daca685b573a5
Kelly Gribel Ayala Torres <ayalagribel39@gmail.com>
2024-08-15 19:24:51 -0600

    Desarrollo de ejercicio de la secuencia fibonacci

--- codigos/ejercicios/ejercicios/Program.cs
+++ codigos/ejercicios/ejercicios/Program.cs
@@ -10,31 +10,33 @@ namespace ejercicios
     {
         static void Main(string[] args)
         {
-            //Uso de matrices. Calcular el suelo de una persona.
-            Console.Write("Sueldo: ");
-            double sueldo = double.Parse(Console.ReadLine()),
-                afp = sueldo * 6.25 / 100,
-                isss = sueldo * 3 / 100;
-            isss = sueldo * 3 / 100,
-                isr = 0;
-            sueldo -= afp;
-
-            double isr = 0;
-            for (int i = 0; i < 5; i++)
+            //obtener la secuencia fibonacci
+            int a = 0, b = 1, c = 1;
+            /*
+             a+b=c
+             0+1=1
+             1+0=1
+             1+1=2
+             2+1=3
+             3+2=5
+             5+3=8
+             8+5=13
+            */
+            while (c < 100)
             {
-                if (sueldo >= matriz[i, 0] && sueldo <= matriz[i, 1])
-                {
-                    isr = (sueldo - (matriz[i, 0] - 0.01)) * matriz[i, 2] / 100 + matriz[i, 3];
-
-                    //Pausa
-                    Console.ReadLine();
-
-
+                Console.WriteLine("{0}+{1}={2}", a, b, c);
+                b = a;
+                a = c;
+                c = a + b;
+            }
+            Console.ReadLine();
 
 
 
 
 
-                }
+
+
+        }
         }
     }
