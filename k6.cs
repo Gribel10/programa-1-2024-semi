03cbca09c5d4f4ca12ab12cf05ef5b610a943da3
Gribel10 <ayalagribel39@gmail.com>
2024-11-26 13:21:30 -0600

    Programación de botones de nuevo y modificar

---
+++ k6.cs
@@ -0,0 +1,40 @@
+﻿  }
+        private void estadoControles(Boolean estado)
+{
+    grbDatosAlumno.Enabled = estado;
+    grbNavegacionAlumno.Enabled = !estado;
+    btnEliminarAlumno.Enabled = !estado;
+    btnBuscarAlumno.Enabled = !estado;
+}
+private void btnNuevoAlumno_Click(object sender, EventArgs e)
+{
+    if (btnNuevoAlumno.Text == "Nuevo")
+    {
+        btnNuevoAlumno.Text = "Guardar";
+        btnModificarAlumno.Text = "Cancelar";
+        estadoControles(true);
+    }
+    else
+    {//Guardar
+        btnNuevoAlumno.Text = "Nuevo";
+        btnModificarAlumno.Text = "Modificar";
+        estadoControles(false);
+    }
+}
+private void btnModificarAlumno_Click(object sender, EventArgs e)
+{
+    if (btnModificarAlumno.Text == "Modificar")
+    {
+        btnNuevoAlumno.Text = "Guardar";
+        btnModificarAlumno.Text = "Cancelar";
+        estadoControles(true);
+    }
+    else
+    {//Cancelar
+        btnNuevoAlumno.Text = "Nuevo";
+        btnModificarAlumno.Text = "Modificar";
+        estadoControles(false);
+    }
+}
+    }
+}
